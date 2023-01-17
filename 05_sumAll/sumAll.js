bb84838de037796170254b4ad724c4740595b8ae
J <118442144+model-map@users.noreply.github.com>
2023-01-17 10:59:57 +0530

    Solve. Use math.max/min() & wrap "if" check at top

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (
+        !(typeof(num1)=='number' &&
+        typeof(num2)=='number' &&
+        num1>=0 &&
+        num2>=0)
+    ) return 'ERROR';
 
+
+    let start=Math.min(num1,num2);
+    let end=Math.max(num1,num2);
+    let result=0;
+    for (let i=start;i<=end;i++) result+=i;
+    return result;
+    
 };
 
 // Do not edit below this line
