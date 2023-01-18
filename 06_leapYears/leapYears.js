7f395885e672a4d99d4f55ddeb658d223348b3fc
J <118442144+model-map@users.noreply.github.com>
2023-01-18 07:38:34 +0530

    Solve. Use nested conditional (ternary) operator

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result=(year%100==0)?((year%400==0)?true:false):((year%4==0)?true:false);
+    return result;
 };
 
 // Do not edit below this line
