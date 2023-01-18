fc268903ad9039521afa4b3b87c4a2ea980dcbb7
J <118442144+model-map@users.noreply.github.com>
2023-01-18 12:14:48 +0530

    Solve. Use array.map to iterate through     - https://www.youtube.com/watch?v=G6J2kl1aVao for understanding map

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(booksDB) {
+    /* let result=[];
+    for (const book of booksDB) result.push(book['title']);
+    return result; */
 
+    // ANOTHER CODE
+    return booksDB.map(book => book.title);
 };
 
 // Do not edit below this line
