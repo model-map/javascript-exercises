ad2475d693c970416a04f56519fdec1a48c0dd03
J <118442144+model-map@users.noreply.github.com>
2023-01-18 08:55:48 +0530

    Solve. Use regex to replace non-alpha chars     - Commented code matches using regex. Replace works better. But there are options.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (string) {
+/*     string=string.toLowerCase();
+    let string_onlyAlphabets='';
+    let alpha=/[a-z]/i;
+    let splitString=string.split('');
+    for (const letter of splitString){
+        if (letter.match(alpha)) string_onlyAlphabets+=letter;
+    }
+    let reverse_string_onlyAlphabets=string_onlyAlphabets.split('').reverse().join('');
+    if (string_onlyAlphabets==reverse_string_onlyAlphabets) return true;
+    return false; */
 
+    // ANOTHER SOLUTION:
+    string=string.toLowerCase().replace(/[^a-z]/gi,'');
+    reverse_string=string.split('').reverse().join('');
+    if (string==reverse_string) return true;
+    return false;
+
+   
 };
 
 // Do not edit below this line
