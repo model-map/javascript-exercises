a033b5ba945fbc02ba058f2cd0b834ea8f179cad
J <118442144+model-map@users.noreply.github.com>
2023-01-18 08:27:03 +0530

    Solve.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,36 @@
-const add = function() {
+const add = function(num1,num2) {
+  return num1+num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1,num2) {
+  return num1-num2;
 	
 };
 
-const sum = function() {
+const sum = function(TheArgs) {
+  let result=0;
+  for (const arg of TheArgs) result+=arg;
+  return result;
 	
 };
 
-const multiply = function() {
+const multiply = function(TheArgs) {
+  let result=1;
+  for (const arg of TheArgs) result*=arg;
+  return result;
 
 };
 
-const power = function() {
+const power = function(num1,num2) {
+  return num1**num2;
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let result=1;
+  for (let i=num;i>=1;i--) result*=i;
+  return result;
 	
 };
 
