9b27dfdded2b17e8e12a32b7a8128c4c1dbea7fb
J <118442144+model-map@users.noreply.github.com>
2023-01-18 13:27:57 +0530

    use array.map() with ternary operators to get age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
+    const currentYear=date.getFullYear();
+
+    // Getting age
+    let age=people.map((person)=>person.yearOfDeath?person.yearOfDeath-person.yearOfBirth:currentYear-person.yearOfBirth);
+    
 
 };
 
