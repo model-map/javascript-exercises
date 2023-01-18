cd29312b500de09029e7773c0a01ea1ffb61110b
J <118442144+model-map@users.noreply.github.com>
2023-01-18 09:40:05 +0530

    Solve. [array].slice() to get [-1] & [-2]     - First solution uses length to get the elements

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(search) {
+    /* if (search<0) return 'OOPS';
+    let newFibonacci=[0,1];
+    let length=newFibonacci.length;
+    for (let i=1;i<=search-1;i++){
+        let newElement=newFibonacci[length-1]+newFibonacci[length-2];
+        newFibonacci.push(newElement);
+        length=newFibonacci.length;
+    }
+    return newFibonacci[search]; */
+
+    // ANOTHER CODE
+    let arrayFibonacci=[0,1];
+    if (search<0) return 'OOPS';
+    for (let i=1;i<=search;i++){
+        let newElement=parseInt(arrayFibonacci.slice(-1))+parseInt(arrayFibonacci.slice(-2,-1));
+        arrayFibonacci.push(newElement)
+    }
+    return arrayFibonacci[search];
 
 };
 
