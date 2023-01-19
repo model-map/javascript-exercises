97ad208987b48dc44d10b173a2a6aeaecc963252
J <118442144+model-map@users.noreply.github.com>
2023-01-19 08:17:39 +0530

    Write a separate fn to shift letters      -Betters readability

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -5,21 +5,20 @@ const caesar = function(password,shift) {
     console.log('Z'.charCodeAt()); //90 */
 
     while (shift<0) shift=(26+shift);
-
-    let caeserArray=password.split('').map(letter =>{
-        if (letter.match(/[a-z]/gi)){
-            let charCode=letter.charCodeAt();
-            let newCharCode=charCode+shift;
-            while ((charCode<=90 && newCharCode>90) || (charCode<=122 && newCharCode>122)) newCharCode-=26;
-            return(String.fromCharCode(newCharCode));
-        }
-        else return(letter);
-    })
-
+    let caeserArray=password.split('').map(letter =>letterShift(letter,shift));
     let caesarPassword=caeserArray.join('');
-    // console.log(`og pass: ${password}\ncaeser pass: ${caesarPassword}`);
     return caesarPassword;
 };
+
+function letterShift(letter,shift){
+    if (letter.match(/[a-z]/gi)){
+        let charCode=letter.charCodeAt();
+        let newCharCode=charCode+shift;
+        while ((charCode<=90 && newCharCode>90) || (charCode<=122 && newCharCode>122)) newCharCode-=26;
+        return(String.fromCharCode(newCharCode));
+    }
+    else return(letter);
+}
 
 // Do not edit below this line
 module.exports = caesar;
